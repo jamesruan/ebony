a9673b68b45730f636ae5cd91f47dd1ed9a020ee
James Ruan <ruanbeihong@gmail.com>
2014-02-05 15:58:12 +0800

    add make.bat for Windows environment

---
+++ stylesheets/make.bat
@@ -0,0 +1,4 @@
+﻿call lessc --source-map=main_ebony.css.map main_ebony.less main_ebony.css 
+call lessc --source-map=main_ivory.css.map main_ivory.less main_ivory.css 
+call lessc --source-map=index_ebony.css.map index_ebony.less index_ebony.css 
+call lessc --source-map=index_ivory.css.map index_ivory.less index_ivory.css 
